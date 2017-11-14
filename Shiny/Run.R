5343691c2f2acbc8d04b0641577b6f02f92bbe62
SebastianS10 <32813376+SebastianS10@users.noreply.github.com>
2017-11-14 15:08:45 +0100

    typo

--- Shiny/Run.R
+++ Shiny/Run.R
@@ -2,7 +2,7 @@
 
 is.installed <- function(mypkg) is.element(mypkg, installed.packages()[,1]) 
 
-needed <- c("jsonlite","xts","ggplot2","dyaphs","PerformanceAnalytics","twitteR","plyr","ROAuth","httr","stringr")
+needed <- c("jsonlite","xts","ggplot2","dygraphs","PerformanceAnalytics","twitteR","plyr","ROAuth","httr","stringr")
 
 is_inst_list <- lapply(needed,is.installed)
 names(is_inst_list) <- needed 
